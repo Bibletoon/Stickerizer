d61e1ecc411bbc739487da417381d4a3a14ff0f4
bibletoon <alex8.ivanov@gmail.com>
2024-06-12 11:07:35 +0300

    Use built in library types

--- src/Telegram/Bot.ts
+++ src/Telegram/Bot.ts
@@ -1,9 +1,7 @@
-import {InlineQuery} from "./InlineQuery";
 import {renderMessage} from "../Generator/Generator";
 import colorsMap from "../Styles/ColorsMap";
-import {UserProfilePhotos} from "./UserProfilePhotos";
-const TelegramBot = require('node-telegram-bot-api');
-
+import {InlineQuery, UserProfilePhotos} from "node-telegram-bot-api";
+const TelegramBot = require("node-telegram-bot-api");
 
 const createBot = (token: String) => {
     const bot = new TelegramBot(token, {polling: true})
@@ -17,12 +15,8 @@ const createBot = (token: String) => {
 
         if (userProfilePhotos.total_count > 0) {
             const photo = userProfilePhotos.photos[0][0]
-            if (photo.file_path && photo.file_path.length !== 0) {
-                avatarUrl = `https://api.telegram.org/file/bot${token}/${userProfilePhotos.photos[0][0].file_path}`
-            } else {
-                const file = await bot.getFile(photo.file_id)
-                avatarUrl = `https://api.telegram.org/file/bot${token}/${file.file_path}`
-            }
+            const file = await bot.getFile(photo.file_id)
+            avatarUrl = `https://api.telegram.org/file/bot${token}/${file.file_path}`
         }
 
         const avatarLoad = performance.now()
