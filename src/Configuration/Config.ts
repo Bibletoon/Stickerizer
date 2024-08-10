d3dcea183b032cf6dcb46de85fd039ed7e422a29
bibletoon <alex8.ivanov@gmail.com>
2024-08-10 10:15:45 +0300

    Feat: modify config

---
+++ src/Configuration/Config.ts
@@ -0,0 +1,19 @@
+﻿// file: state/config.ts
+import { fromJSONFile } from "@mrspartak/config";
+import * as z from "zod"
+
+const configSchema = z.object({
+    bot: z.object({
+        token: z.string()
+    }),
+    app: z.object({
+        logLevel: z.string().optional().default('info')
+    })
+})
+
+const readConfig = async (path: string | string[]) => await fromJSONFile({
+    path: path,
+    schema: configSchema
+});
+
+export default readConfig
--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,24 @@ import AvatarLoader from "./Telegram/AvatarLoader";
 import InMemoryCacheProvider from "./Cache/InMemoryCacheProvider";
 import pino, {Logger} from "pino"
 import LocalTimeMeasurer from "./TimeMeasure/LocalTimeMeasurer";
+import readConfig from "./Configuration/Config";
 
 const TelegramBot = require("node-telegram-bot-api");
 
 process.env.NTBA_FIX_350 = "1";
-const token : string = process.env.TOKEN || '';
 
 void async function main() {
+    const config = await readConfig(["config.json", "config.secrets.json"])
+    
     const logger : Logger = pino()
-    logger.level = process.env.LOG_LEVEL || 'info'
+    logger.level = config.app.logLevel;
     const stickerGenerator = await StickerGenerator.create();
-    const bot = new TelegramBot(token, {
+    const bot = new TelegramBot(config.bot.token, {
         polling: true,
         filepath: false
     })
     const cache = new InMemoryCacheProvider<number, string>()
-    const avatarLoader = new AvatarLoader(bot, token, cache);
+    const avatarLoader = new AvatarLoader(bot, config.bot.token, cache);
     
     const handler = new InlineQueryHandler(
         bot, 
